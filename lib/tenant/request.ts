53c3b65315e5e60398db2351fef0bffc292d6845
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-03-14 16:16:29 +0700

    feat(tenant): Support profile repo

--- lib/tenant/request.ts
+++ lib/tenant/request.ts
@@ -13,11 +13,14 @@ export const parseTenantRequest = (
 	page: PageRequest | undefined
 ): ContentRequest => {
 	if (page === undefined) throw Error("request is undefined");
-	const { slug, tenant } = page;
+	const { tenant } = page;
 	if (tenant === undefined) throw Error("tenant is undefined");
-	if (slug === undefined) throw Error("slug is undefined");
 
 	const owner = tenant;
+	// Use special profile repo if there's no slug (no repo specified)
+	// https://docs.github.com/en/account-and-profile/setting-up-and-managing-your-github-profile/customizing-your-profile/managing-your-profile-readme
+	const slug = page.slug ?? [owner];
+
 	const [repo, ...parts] = slug;
 	const path = parts.join("/");
 
--- pages/_tenants/[tenant]/index.tsx
+++
@@ -1,7 +0,0 @@
-import { NextPage } from "next";
-
-const TenantPage: NextPage<{}> = () => {
-	return <div>Home</div>;
-};
-
-export default TenantPage;
