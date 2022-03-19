2601f453bac69d2440a68dfa16b260626fccfc7b
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-03-19 10:39:03 +0700

    fix(blog): Fix GitHub blog entry link

--- lib/blog-github/dir/entry.tsx
+++ lib/blog-github/dir/entry.tsx
@@ -1,5 +1,6 @@
 import { BlogDirEntry, BlogDirEntryProps } from "@/lib/blog/dir/entry";
 import { BlogGitHubRequest } from "../type";
+import nodepath from "path";
 
 type Props = BlogDirEntryProps<BlogGitHubRequest>;
 
@@ -7,9 +8,7 @@ const getHref = (props: Props): string => {
 	const { entry, request } = props;
 	// don't need "user" here because we redirect inside subdomain
 	const { repo, path } = request;
-	const href = [repo, path, entry.name]
-		.filter((part) => part !== "") // avoid double slash
-		.join("/");
+	const href = `/${nodepath.join(repo, path, entry.name)}`;
 	return href;
 };
 
