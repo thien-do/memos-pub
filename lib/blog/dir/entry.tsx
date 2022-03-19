b0304017bd4c8de7ef156a6ac9cda465a8a3c50c
Thinh Le <lqthinh07@gmail.com>
2022-03-19 23:54:11 +0700

    feat(blog): support date in title

--- lib/blog/dir/entry.tsx
+++ lib/blog/dir/entry.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import * as type from "../type";
+import { getEntryInfo } from "./utils";
 
 export interface BlogDirEntryProps<R> {
 	entry: type.BlogDirEntry;
@@ -12,11 +13,13 @@ interface Props<R> extends BlogDirEntryProps<R> {
 
 export const BlogDirEntry = <R,>(props: Props<R>): JSX.Element => {
 	const isDir = props.entry.type === "dir";
+	const { date, title } = getEntryInfo(props.entry.name);
 	return (
 		<li className={isDir ? "list-[disclosure-closed]" : ""}>
 			<Link href={props.getHref(props)}>
 				<a target="_self" className="font-normal no-underline">
 					{props.entry.name}
+					<span className="text-stone-500">{date}</span><span>{title}</span>
 				</a>
 			</Link>
 		</li>
