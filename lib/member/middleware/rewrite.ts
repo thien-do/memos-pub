291fccf4f1199f50adf683c2d45a4a66629d9c3a
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-04-06 09:51:39 +0700

    member: add huydx

--- lib/member/middleware/rewrite.ts
+++ lib/member/middleware/rewrite.ts
@@ -6,6 +6,7 @@ const MEMBERS: { from: string; to: string }[] = [
 	{ from: "thien.do", to: "thien-do/thien-do" },
 	{ from: "notes.ancaois.me", to: "kcjpop/kcjpop" },
 	{ from: "mquy.dev", to: "mquy/mquy" },
+	{ from: "huydx.dev", to: "huydx/notes" },
 	{ from: "notes.snacky.blog", to: "huytd/git-notes" },
 	{ from: "notes.minhle.space", to: "monodyle/monodyle" },
 ];
