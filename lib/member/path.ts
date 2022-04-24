897fee41db74bb93ae970a063c62fe37b9db53ad
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-04-24 09:09:41 +0700

    fix(member): Fix huydx.dev

--- lib/member/path.ts
+++ lib/member/path.ts
@@ -2,7 +2,7 @@ export const MEMBER_PATHS: { from: string; to: string }[] = [
 	{ from: "thien.do", to: "thien-do/thien-do" },
 	{ from: "ancaois.me", to: "kcjpop/kcjpop" },
 	{ from: "mquy.dev", to: "mquy/mquy" },
-	{ from: "huydx.dev", to: "huydx/notes" },
+	{ from: "notes.huydx.dev", to: "huydx/notes" },
 	{ from: "notes.snacky.blog", to: "huytd/git-notes" },
 	{ from: "notes.minhle.space", to: "monodyle/monodyle" },
 	{ from: "www.chars.fyi", to: "thien-do/chars.fyi" },
