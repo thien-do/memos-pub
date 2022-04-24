a8dbdb244d5253d0bd0c0a4f6a3a5aae5764cf78
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-04-24 09:50:57 +0700

    fix(blog): Remove custom entries option

--- lib/blog/list/config.ts
+++ lib/blog/list/config.ts
@@ -11,34 +11,8 @@ export interface BlogListConfig {
 	 */
 	readme?: "show" | "only" | "hide";
 	/**
-	 * Customize the entry list. Use this to control the order of blog posts
-	 * or display different titles than file names.
-	 *
-	 * If not defined, memos.pub uses the original file names to list and sort
-	 * the entry list
-	 */
-	entries?: BlogListConfigEntry[];
-	/**
 	 * Recognize the date in an entry's name to improve entry list (e.g.
 	 * sorting and date column)
 	 */
 	dateFormat?: BlogEntryDateFormat;
-}
-
-export interface BlogListConfigEntry {
-	/**
-	 * The name to be displayed in the entry list. It can include whitespace,
-	 * punctuation marks or any special characters. This is usually the
-	 * article's title.
-	 *
-	 * E.g. "This, That, and the Other! An Outré Collection"
-	 */
-	name: string;
-	/**
-	 * The actual name of the markdown file. It is used in URLs and to fetch
-	 * the file content. This is usually a short, URL-friendly file name.
-	 *
-	 * E.g. "this-that-other-outre-collection.md"
-	 */
-	path: string;
 }
--- lib/github/fetch/blog/list/entry.ts
+++ lib/github/fetch/blog/list/entry.ts
@@ -1,6 +1,6 @@
 import { parseBlogEntryDate } from "@/lib/blog/entry/date";
 import { BlogEntry } from "@/lib/blog/entry/type";
-import { BlogListConfig, BlogListConfigEntry } from "@/lib/blog/list/config";
+import { BlogListConfig } from "@/lib/blog/list/config";
 import { BlogList } from "@/lib/blog/list/type";
 import { GitHubDir, GitHubDirEntry } from "../../type";
 
@@ -10,22 +10,8 @@ interface Props {
 }
 
 export const getGitHubBlogEntries = (props: Props): BlogList["entries"] => {
-	if (props.config?.entries) {
-		return props.config.entries.map(fromConfig(props));
-	} else {
-		return props.dir.filter(isFileValid).map(fromFile(props));
-	}
+	return props.dir.filter(isFileValid).map(fromFile(props));
 };
-
-const fromConfig =
-	(props: Props) =>
-	(raw: BlogListConfigEntry): BlogEntry => {
-		const name = raw.name;
-		const format = props.config?.dateFormat ?? "yyyy-mm-dd ";
-		const { date, rest } = parseBlogEntryDate({ format, name });
-		const title = date === null ? name : rest;
-		return { date, name: raw.path, title, type: "post" };
-	};
 
 const isFileValid = (entry: GitHubDirEntry): boolean => {
 	const { name, type } = entry;
