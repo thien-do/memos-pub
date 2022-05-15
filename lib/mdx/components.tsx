9a1ce76ba654ebd3cdf8fbb28a0945483e0a5789
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-05-15 13:59:13 +0700

    feat(mdx): Improve mdx links
    Support client-side routing

--- lib/mdx/components.tsx
+++ lib/mdx/components.tsx
@@ -1,10 +1,19 @@
 import Head from "next/head";
 import { MDXComponents } from "mdx/types";
+import Link from "next/link";
+
+// To have client-side routing
+const CustomLink: MDXComponents["a"] = (props) => (
+	<Link href={props.href!}>
+		<a {...props} />
+	</Link>
+);
 
 /**
  * Use Next's components in MDX (e.g. Head)
  */
 export const mdxComponents: MDXComponents = {
+	a: CustomLink,
 	// https://github.com/mdx-js/mdx/discussions/1921
 	head: Head as any,
 };
