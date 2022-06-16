05c86cb4d1b751466d4b6d41797a8aeddd8e1b1f
Thien Do <5953369+thien-do@users.noreply.github.com>
2022-06-16 10:45:32 +0700

    member: Remove keva.dev

--- lib/member/list.ts
+++ lib/member/list.ts
@@ -9,7 +9,6 @@ export const MEMBER_LIST: MemberPath[] = [
 	{ from: "mquy.dev", to: "mquy/mquy" },
 	{ from: "notes.huydx.dev", to: "huydx/notes" },
 	{ from: "www.chars.fyi", to: "thien-do/chars.fyi" },
-	{ from: "blog.keva.dev", to: "keva-dev/blog" },
 	{ from: "memo.dev-random.io", to: "unrealhoang/unrealhoang.github.io" },
 	{ from: "j.nguyenrk.com", to: "huyng12/30-days-japanese" },
 	{ from: "notes.vanhtuan0409.com", to: "vanhtuan0409/notes" },
