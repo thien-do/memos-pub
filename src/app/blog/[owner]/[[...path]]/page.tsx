33c297fb613e3e7f65e40f971cd315e9bf2f5c59
Thien Do <5953369+thien-do@users.noreply.github.com>
2026-09-06 18:43:19 +0700

    1

--- src/app/blog/[owner]/[[...path]]/page.tsx
+++ src/app/blog/[owner]/[[...path]]/page.tsx
@@ -1,7 +1,15 @@
+import type { Metadata } from "next";
 import type { ReactElement } from "react";
+import { getBlogMetadata } from "@/blog/metadata";
 import { BlogPage } from "@/blog/page";
 
 type Props = PageProps<"/blog/[owner]/[[...path]]">;
+
+export async function generateMetadata(props: Props): Promise<Metadata> {
+  const { owner } = await props.params;
+
+  return getBlogMetadata({ owner });
+}
 
 export default async function Page(props: Props): Promise<ReactElement> {
   const { owner, path } = await props.params;
