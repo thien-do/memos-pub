dc898d744793108355f207c5b1e1c869a81c8d2b
Thien Do <5953369+thien-do@users.noreply.github.com>
2026-09-13 12:55:51 +0700

    Switch to TanStack Markdown (#73)

--- src/mark/file.tsx
+++ src/mark/file.tsx
@@ -1,23 +1,16 @@
+import type { UrlTransform } from "@tanstack/markdown";
+import { Markdown } from "@tanstack/markdown/react";
 import { cacheLife } from "next/cache";
 import type { ReactElement } from "react";
-import { defaultUrlTransform, MarkdownAsync } from "react-markdown";
-import remarkGfm from "remark-gfm";
 import { dropMarkAutoMD } from "./url";
 
-function transformUrl(url: string, key: string): string {
-  const href = key === "href" ? dropMarkAutoMD(url) : url;
-  // Keep the default important transform
-  return defaultUrlTransform(href);
-}
+const transformUrl: UrlTransform = (_url, kind, defaultUrl) =>
+  kind === "link" ? dropMarkAutoMD(defaultUrl) : defaultUrl;
 
 export async function MarkFile(props: { text: string }): Promise<ReactElement> {
   "use cache: remote";
   cacheLife("days");
   const { text } = props;
 
-  return (
-    <MarkdownAsync remarkPlugins={[remarkGfm]} urlTransform={transformUrl}>
-      {text}
-    </MarkdownAsync>
-  );
+  return <Markdown urlTransform={transformUrl}>{text}</Markdown>;
 }
